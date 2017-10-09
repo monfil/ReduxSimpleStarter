867c5b9b1ad4dd6e2c1eb876212a4273b0ef7478
Fabiola Monfil <fmonfil@intelimetrica.com>
2017-10-09 15:37:52 -0500

    Set up

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyAsNyhf8U_7f1AxnG6G61pHchmlRaXvSho';
+
+// Create a new component. This component should produe some HTML.
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.videos[0]} />
+				<VideoList videos ={this.state.videos} />
+			</div>
+		);
+	}
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM).
+ReactDOM.render(<App />, document.querySelector('.container'));
